4d3ecac1f2ca9c4f643a0116805beee88b9e0f85
wellshs <wellshs@kaist.ac.kr>
2022-08-18 22:46:37 +0900

    Remove unused code

--- main.py
+++ main.py
@@ -8,10 +8,6 @@ env = gym.make("ALE/Breakout-v5", frameskip=1)
 env = gym.wrappers.AtariPreprocessing(env)
 env = gym.wrappers.FrameStack(env, 4)
 
-evaluate_env = gym.make("ALE/Breakout-v5", frameskip=1, render_mode="human")
-evaluate_env = gym.wrappers.AtariPreprocessing(evaluate_env)
-evaluate_env = gym.wrappers.FrameStack(evaluate_env, 4)
-
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = Network(action_space=env.action_space).to(device)
